147b27f40b6768a51608bfc1123b76ded16db970
Shivank23 <shivankpandey23@gmail.com>
2020-12-12 18:28:27 +0700

    Updates

---
+++ precache-manifest.85a3fbba7d40aa95d9dcd7429ebbe6fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab3c36a77baac928ac644b50dadeb3df",
+    "url": "/Netflix-clone/index.html"
+  },
+  {
+    "revision": "5d3363dfe187173bed67",
+    "url": "/Netflix-clone/static/css/main.7b1df042.chunk.css"
+  },
+  {
+    "revision": "032e1cfce5a3c31fae71",
+    "url": "/Netflix-clone/static/js/2.a853ec82.chunk.js"
+  },
+  {
+    "revision": "894f61387b383e6e2e0543fb120b4656",
+    "url": "/Netflix-clone/static/js/2.a853ec82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d3363dfe187173bed67",
+    "url": "/Netflix-clone/static/js/main.e40ecf96.chunk.js"
+  },
+  {
+    "revision": "37005dd9bfd48c744798",
+    "url": "/Netflix-clone/static/js/runtime-main.2122e3aa.js"
+  }
+]);
